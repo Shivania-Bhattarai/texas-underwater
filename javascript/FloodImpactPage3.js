0449cdb7ec71c4f8a2bb8a53af3027a6566e5f0a
mandip-pro <mandipgyawali7@gmail.com>
2025-08-05 22:43:02 +0545

    worked on page2

--- javascript/FloodImpactPage3.js
+++ javascript/FloodImpactPage3.js
@@ -1,28 +1,5 @@
-const overlayClipMax = 100; // Maximum % for full reveal
+const overlayClipMax = 100;
 const overlayClipMin = 0;
-// v1
-// const overlayClipMax = 100; // Maximum % for full reveal
-// const overlayClipMin = 0;
-
-// export const updatePage3OverlayClip = (
-//   currentPage,
-//   pageElements,
-//   indicatorTop,
-//   maxTop
-// ) => {
-//   if (currentPage !== 2) return;
-//   const page3 = pageElements[2];
-//   const overlay = page3.querySelectorAll(".overlay");
-
-//   // Calculate how much of the overlay should be revealed
-//   const clipPercentage = 100 - (indicatorTop / maxTop) * 100;
-//   const clamped = Math.max(
-//     overlayClipMin,
-//     Math.min(overlayClipMax, clipPercentage)
-//   );
-
-//   overlay.style.clipPath = `inset(0 0 0 ${clamped}%)`;
-// };
 
 
 export const updatePage3OverlayClip = (currentPage, pageElements, indicatorTop, maxTop) => {
